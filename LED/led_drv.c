5570e4bc609b53d6d014199cbe9147c40c7e9430
mculover666 <2412828003@qq.com>
2019-07-10 13:32:09 +0800

    add independent led driver

---
+++ LED/led_drv.c
@@ -0,0 +1,33 @@
+/**
+ * @Copyright 			(c) 2019,mculover666 All rights reserved	
+ * @filename  			led.c
+ * @breif						independent LED driver file
+ * @version
+ *            			1.0    mculover666    2019/7/10
+ */
+
+#include "led_drv.h"
+
+/**
+  * @brief  LED GPIO Initialization Function
+  * @param  None
+  * @retval None
+  */
+void LED_init(void)
+{
+	
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin*/
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+	
+	/*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
+}
---
+++ LED/led_drv.h
@@ -0,0 +1,21 @@
+#ifndef	_LED_DRV_H_
+#define _LED_DRV_H_
+/**
+ * @Copyright (c) 2019,mculover666 
+              All rights reserved	
+ * @filename  led.h
+ * @breif     independent LED driver file
+ * @version
+ *            1.0    mculover666    2019/7/10
+ */
+
+#include "stm32l4xx_hal.h"
+
+#define LED_PORT	GPIOC
+#define	LED_PIN		GPIO_PIN_13
+
+#define LED(n)	 (n?HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET):HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET));
+
+void LED_init(void);
+
+#endif /* _LED_DRV_H_ */
