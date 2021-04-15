4b219f3840b6f72fe0a98587de6a4b462069f0c5
mculover666 <2412828003@qq.com>
2021-04-15 15:53:54 +0800

    fix the bug in at24c02 driver

--- AT24C02-IIC/at24c02_i2c_drv.c
+++ AT24C02-IIC/at24c02_i2c_drv.c
@@ -1,109 +1,149 @@
 /**
- * @Copyright 			(c) 2019,mculover666 All rights reserved	
- * @filename  			at24c02_i2c_drv.c
- * @breif				Drive AT24C02(EEPROM) based on iic1 commucation interface
- * @version
- *            			v1.0    完成基本驱动程序              mculover666    2019/7/15
- * @note            	移植说明（非常重要）：	
- *						1. 修改at24c02_i2c_drv.h文件中的AT24C02读写地址即可；
- * 						2. 此驱动程序需要STM32CubeMX生成的I2C初始化文件i2c.h和i2c.c支持。
+ * @Copyright   (c) 2019,mculover666 All rights reserved	
+ * @filename    at24C02_i2c_drv.c
+ * @breif       Drive AT24C02(EEPROM) based on stm32 iic peripheral
+ * @changelog   v1.0    mculover666     2019/7/15
+ *              v1.1    mculover666     2021/4/15  (add WRITE_CYCLE_TIME_MS)
  */
 
-#include "at24c02_i2c_drv.h"
+#include "AT24C02_i2c_drv.h"
 #include "i2c.h"
 
 /**
- * @brief		AT24C02任意地址写一个字节数据
- * @param		addr —— 写数据的地址（0-255）
- * @param		dat  —— 存放写入数据的地址
- * @retval		成功 —— HAL_OK
+ * @brief       write a byte to any address
+ * @param[in]	addr    address to write  
+ * @param[in]   data    the pointer of data to write
+ * @return		errcode
+ * @retval      0       success
+ * @retval      -1      fail
 */
-uint8_t At24c02_Write_Byte(uint16_t addr, uint8_t* dat)
+int AT24C02_Write_Byte(uint16_t addr, uint8_t* dat)
 {
-		return HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, 1, 0xFFFF);
+    int ret;
+    
+    HAL_Delay(WRITE_CYCLE_TIME_MS);
+    
+    ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, 1, 100);
+    
+    return ret == HAL_OK ? 0 : -1;
 }
+
 /**
- * @brief		AT24C02任意地址读一个字节数据
- * @param		addr —— 读数据的地址（0-255）
- * @param		read_buf —— 存放读取数据的地址
- * @retval		成功 —— HAL_OK
+ * @brief       read a byte from any address
+ * @param[in]	addr        address to rad  
+ * @param[out]  read_buf    the pointer of buffer to save read data
+ * @return		errcode
+ * @retval      0       success
+ * @retval      -1      fail
 */
-uint8_t At24c02_Read_Byte(uint16_t addr, uint8_t* read_buf)
+int AT24C02_Read_Byte(uint16_t addr, uint8_t* read_buf)
 {
-		return HAL_I2C_Mem_Read(&hi2c1, AT24C02_ADDR_READ, addr, I2C_MEMADD_SIZE_8BIT, read_buf, 1, 0xFFFF);
+    int ret;
+
+    HAL_Delay(WRITE_CYCLE_TIME_MS);
+    
+    ret = HAL_I2C_Mem_Read(&hi2c1, AT24C02_ADDR_READ, addr, I2C_MEMADD_SIZE_8BIT, read_buf, 1, 100);
+    
+    return ret == HAL_OK ? 0 : -1;
 }
+
 /**
- * @brief		AT24C02任意地址连续写多个字节数据
- * @param		addr —— 写数据的地址（0-255）
- * @param		dat  —— 存放写入数据的地址
- * @retval		成功 —— HAL_OK
+ * @brief       write amount bytes to any address
+ * @param[in]	addr    address to write
+ * @param[in]   data    the pointer of data buffer to write
+ * @return		errcode
+ * @retval      0       success
+ * @retval      else      fail
 */
-uint8_t At24c02_Write_Amount_Byte(uint16_t addr, uint8_t* dat, uint16_t size)
+int AT24C02_Write_Amount_Byte(uint16_t addr, uint8_t* dat, uint16_t size)
 {
-		uint8_t i = 0;
-		uint16_t cnt = 0;		//写入字节计数
-		
-		/* 对于起始地址，有两种情况，分别判断 */
-		if(0 == addr % 8 )
-		{
-			/* 起始地址刚好是页开始地址 */
-			
-			/* 对于写入的字节数，有两种情况，分别判断 */
-			if(size <= 8)
-			{
-				//写入的字节数不大于一页，直接写入
-				return HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, size, 0xFFFF);
-			}
-			else
-			{
-				//写入的字节数大于一页，先将整页循环写入
-				for(i = 0;i < size/8; i++)
-				{
-						HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], 8, 0xFFFF);
-						addr += 8;
-						cnt += 8;
-				}
-				//将剩余的字节写入
-				return HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], size - cnt, 0xFFFF);
-			}
-		}
-		else
-		{
-			/* 起始地址偏离页开始地址 */
-			/* 对于写入的字节数，有两种情况，分别判断 */
-			if(size <= (8 - addr%8))
-			{
-				/* 在该页可以写完 */
-				return HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, size, 0xFFFF);
-			}
-			else
-			{
-				/* 该页写不完 */
-				//先将该页写完
-				cnt += 8 - addr%8;
-				HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, cnt, 0xFFFF);
-				addr += cnt;
-				
-				//循环写整页数据
-				for(i = 0;i < (size - cnt)/8; i++)
-				{
-						HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], 8, 0xFFFF);
-						addr += 8;
-						cnt += 8;
-				}
-				
-				//将剩下的字节写入
-				return HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], size - cnt, 0xFFFF);
-			}			
-		}
+    int ret;
+    uint16_t i = 0;
+    uint16_t cnt = 0;
+    uint16_t page = 0;
+    
+    if (0 == addr % 8) {    // whether the addr is the starting address of the page
+        if (size <= 8) {    //whether the number of bytes to be written is less than one page
+            HAL_Delay(WRITE_CYCLE_TIME_MS);
+            ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, size, 100);
+            if (ret != HAL_OK) {
+                return -1;
+            }
+        } else {
+            page = size / 8;
+            for (i = 0;i < page; i++) {
+                HAL_Delay(WRITE_CYCLE_TIME_MS);
+                ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], 8, 100);
+                if (ret != HAL_OK) {
+                    return -2;
+                }
+                addr += 8;
+                cnt += 8;
+            }
+            if (cnt < size) {     //write remain bytes
+                HAL_Delay(WRITE_CYCLE_TIME_MS);
+                ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], size - cnt, 100);
+                if (ret != HAL_OK) {
+                    return -3;
+                }
+            }
+        }
+    } else {    //the address deviates from the starting page address
+        if (size <= (8 - addr % 8)) {     //whether we can finish writing on this page
+            HAL_Delay(WRITE_CYCLE_TIME_MS);
+            ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, size, 100);
+            if (ret != HAL_OK) {
+                return -4;
+            }
+        } else {    //can't finish on this page
+            //finish the page first
+            cnt += 8 - addr % 8;
+            HAL_Delay(WRITE_CYCLE_TIME_MS);
+            ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, dat, cnt, 100);
+            if (ret != HAL_OK) {
+                return -5;
+            }
+            addr += cnt;
+            
+            //loops to write the entire page of data
+            page = (size - cnt) / 8;
+            for (i = 0;i < page; i++) { 
+                HAL_Delay(WRITE_CYCLE_TIME_MS);
+                ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], 8, 100);
+                if (ret != HAL_OK) {
+                    return -6;
+                }
+                addr += 8;
+                cnt += 8;
+            }
+            if (cnt < size) {     //write remain bytes
+                HAL_Delay(WRITE_CYCLE_TIME_MS);
+                ret = HAL_I2C_Mem_Write(&hi2c1, AT24C02_ADDR_WRITE, addr, I2C_MEMADD_SIZE_8BIT, &dat[cnt], size - cnt, 100);
+                if (ret != HAL_OK) {
+                    return -7;
+                }
+            }
+        }			
+    }
+    
+    return 0;
 }
+
 /**
- * @brief		AT24C02任意地址连续读多个字节数据
- * @param		addr —— 读数据的地址（0-255）
- * @param		dat  —— 存放读出数据的地址
- * @retval		成功 —— HAL_OK
+ * @brief       read amount bytes from any address
+ * @param[in]	addr        address to read  
+ * @param[out]  read_buf    the pointer of buffer to save read data
+ * @return		errcode
+ * @retval      0       success
+ * @retval      -1      fail
 */
-uint8_t At24c02_Read_Amount_Byte(uint16_t addr, uint8_t* recv_buf, uint16_t size)
+int AT24C02_Read_Amount_Byte(uint16_t addr, uint8_t* recv_buf, uint16_t size)
 {
-	return HAL_I2C_Mem_Read(&hi2c1, AT24C02_ADDR_READ, addr, I2C_MEMADD_SIZE_8BIT, recv_buf, size, 0xFFFF);
+    int ret;
+    
+    HAL_Delay(WRITE_CYCLE_TIME_MS);
+    
+	ret = HAL_I2C_Mem_Read(&hi2c1, AT24C02_ADDR_READ, addr, I2C_MEMADD_SIZE_8BIT, recv_buf, size, 100);
+    
+    return ret == HAL_OK ? 0 : -1;
 }
--- AT24C02-IIC/at24c02_i2c_drv.h
+++ AT24C02-IIC/at24c02_i2c_drv.h
@@ -1,24 +1,26 @@
 /**
- * @Copyright 			(c) 2019,mculover666 All rights reserved	
- * @filename  			at24c02_i2c_drv.h
- * @breif				Drive AT24C02(EEPROM) based on iic1 commucation interface
- * @version
- *            			v1.0    完成基本驱动程序              mculover666    2019/7/15
- * @note                移植说明（非常重要）：	
- *						1. 修改at24c02_i2c_drv.h文件中的AT24C02读写地址即可；
- * 					    2. 此驱动程序需要STM32CubeMX生成的I2C初始化文件i2c.h和i2c.c支持。
+ * @Copyright   (c) 2019,mculover666 All rights reserved	
+ * @filename    at24C02_i2c_drv.c
+ * @breif       Drive AT24C02(EEPROM) based on stm32 iic peripheral
+ * @changelog   v1.0    mculover666     2019/7/15
+ *              v1.1    mculover666     2021/4/15  (add WRITE_CYCLE_TIME_MS)
  */
 #ifndef	_AT24C02_I2C_DRV_H_
 #define	_AT24C02_I2C_DRV_H_
 
 #include "stm32l4xx_hal.h"
 
+/* slave device address of eeprom */
 #define	AT24C02_ADDR_WRITE	0xA0
 #define	AT24C02_ADDR_READ	0xA1
 
-uint8_t At24c02_Write_Byte(uint16_t addr, uint8_t* dat);
-uint8_t At24c02_Read_Byte(uint16_t addr, uint8_t* read_buf);
-uint8_t At24c02_Write_Amount_Byte(uint16_t addr, uint8_t* dat, uint16_t size);
-uint8_t At24c02_Read_Amount_Byte(uint16_t addr, uint8_t* recv_buf, uint16_t size);
+/* delay time between write and write or between write and read, value is 5-10ms generally */
+#define WRITE_CYCLE_TIME_MS 5
+
+
+int AT24C02_Write_Byte(uint16_t addr, uint8_t* dat);
+int AT24C02_Read_Byte(uint16_t addr, uint8_t* read_buf);
+int AT24C02_Write_Amount_Byte(uint16_t addr, uint8_t* dat, uint16_t size);
+int AT24C02_Read_Amount_Byte(uint16_t addr, uint8_t* recv_buf, uint16_t size);
 
 #endif	/* _AT24C02_I2C_DRV_H_ */
