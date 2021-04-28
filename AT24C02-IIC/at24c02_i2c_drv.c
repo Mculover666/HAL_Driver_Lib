4d7493d61f39b0b199b2bbb002fa1280614b7dcd
mculover666-3 <mculover666-3@qq.com>
2021-04-28 17:13:38 +0800

    improve at24c02 driver

--- AT24C02-IIC/at24c02_i2c_drv.c
+++ AT24C02-IIC/at24c02_i2c_drv.c
@@ -3,7 +3,8 @@
  * @filename    at24C02_i2c_drv.c
  * @breif       Drive AT24C02(EEPROM) based on stm32 iic peripheral
  * @changelog   v1.0    mculover666     2019/7/15
- *              v1.1    mculover666     2021/4/15  (add WRITE_CYCLE_TIME_MS)
+ *              v1.1    mculover666     2021/4/15   (add WRITE_CYCLE_TIME_MS)
+ *              v1.2    mculover666     2021/4/28   (add value operation)
  */
 
 #include "AT24C02_i2c_drv.h"
@@ -146,4 +147,54 @@ int AT24C02_Read_Amount_Byte(uint16_t addr, uint8_t* recv_buf, uint16_t size)
 	ret = HAL_I2C_Mem_Read(&hi2c1, AT24C02_ADDR_READ, addr, I2C_MEMADD_SIZE_8BIT, recv_buf, size, 100);
     
     return ret == HAL_OK ? 0 : -1;
+}
+
+/**
+ * @brief       write value to any address
+ * @param[in]	addr    address to write
+ * @param[in]   ptr     the pointer of value to write
+ * @return		errcode
+ * @retval      0       success
+ * @retval      else      fail
+*/
+int AT24C02_Write_Value(uint16_t addr, void *ptr, uint16_t size)
+{
+    int ret;
+    
+    if (!ptr) {
+        return -1;
+    }
+  
+    ret = AT24C02_Write_Amount_Byte(addr, (uint8_t*)ptr, size);
+    
+    if (ret < 0) {
+        return ret;
+    }
+    
+    return 0;
+}
+
+/**
+ * @brief       read value from any address
+ * @param[in]	addr    address to write
+ * @param[in]   ptr     the pointer of value to read
+ * @return		errcode
+ * @retval      0       success
+ * @retval      else      fail
+*/
+int AT24C02_Read_Value(uint16_t addr, void *ptr, uint16_t size)
+{
+    int ret;
+    
+    if (!ptr) {
+        return -1;
+    }
+  
+    ret = AT24C02_Read_Amount_Byte(addr, (uint8_t*)ptr, size);
+    
+    if (ret < 0) {
+        return ret;
+    }
+    
+    return 0;
 }
--- AT24C02-IIC/at24c02_i2c_drv.h
+++ AT24C02-IIC/at24c02_i2c_drv.h
@@ -3,8 +3,10 @@
  * @filename    at24C02_i2c_drv.c
  * @breif       Drive AT24C02(EEPROM) based on stm32 iic peripheral
  * @changelog   v1.0    mculover666     2019/7/15
- *              v1.1    mculover666     2021/4/15  (add WRITE_CYCLE_TIME_MS)
+ *              v1.1    mculover666     2021/4/15   (add WRITE_CYCLE_TIME_MS)
+ *              v1.2    mculover666     2021/4/28   (add value operation)
  */
+
 #ifndef	_AT24C02_I2C_DRV_H_
 #define	_AT24C02_I2C_DRV_H_
 
@@ -22,5 +24,7 @@ int AT24C02_Write_Byte(uint16_t addr, uint8_t* dat);
 int AT24C02_Read_Byte(uint16_t addr, uint8_t* read_buf);
 int AT24C02_Write_Amount_Byte(uint16_t addr, uint8_t* dat, uint16_t size);
 int AT24C02_Read_Amount_Byte(uint16_t addr, uint8_t* recv_buf, uint16_t size);
+int AT24C02_Write_Value(uint16_t addr, void *ptr, uint16_t size);
+int AT24C02_Read_Value(uint16_t addr, void *ptr, uint16_t size);
 
 #endif	/* _AT24C02_I2C_DRV_H_ */
